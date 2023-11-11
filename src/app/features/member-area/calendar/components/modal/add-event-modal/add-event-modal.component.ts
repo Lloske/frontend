ef6193c57f9d8767e53f802e23b43054b910dfda
AntoTilm <antoine.tilman@gmail.com>
2023-11-11 12:05:31 +0100

    Création du formulaire dans add-event-component

--- src/app/features/member-area/calendar/components/modal/add-event-modal/add-event-modal.component.ts
+++ src/app/features/member-area/calendar/components/modal/add-event-modal/add-event-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-add-event-modal',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-event-modal.component.scss']
 })
 export class AddEventModalComponent {
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  addEventForm = this.formBuilder.group({
+    shiftStartDate: [""], // Remplacer l'input html par un selecteur automatique
+    shiftEndDate: [""], // Remplacer l'input html par un selecteur automatique
+    shiftStartTime: [""],
+    shiftendTime: [""],
+    shiftPause: [""],
+    shiftTeam: [""],
+    shiftMeal: [""],
+    shiftNote: [""],
+  })
+
+  onSubmit(){
+    console.log(this.addEventForm.value);    
+  }
+
 
 }
