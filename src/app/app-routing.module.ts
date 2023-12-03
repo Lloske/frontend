0631f098e441626a11d08ceb4bd3da3bcc9f3ec5
AntoTilm <antoine.tilman@gmail.com>
2023-12-03 12:59:05 +0100

    Je sais pas si il faut charger le composant public-area-component au niveau du parent ou dans les enfant. HELP PLZ

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,12 +10,12 @@ const routes: Routes = [
   //QUESTION: !!!!!!! VERIFIER SI IL FAUT CHARGER OU NON LE COMPONENT DANS LE PATH ET POURQUOI? !!!!!!!!!!!!!!!!!!
   
   { path : "", redirectTo: "/public-area", pathMatch : "full" }, // Redirige la racine de l'application vers "/public-area" avec une correspondance de chemin exacte (pas pathMath: "prefix" sinon tout ce qui se site apres la racine "/"" redirige vers "/public-area").
-  { path : "member-area", component : MemberAreaComponent, loadChildren : () => import("./features/member-area/member-area.module").then(m => m.MemberAreaModule) }, 
+  { path : "public-area" , component : PublicAreaComponent, loadChildren : () => import("./features/public-area/public-area.module").then(m => m.PublicAreaModule) },
+  { path : "member-area", loadChildren : () => import("./features/member-area/member-area.module").then(m => m.MemberAreaModule) }, 
   // Ne pas mettre de "component : Member-area-composant" entre le path et le loadchildren si on veux met une route vide dans le composant lazy loader. 
   // Si on met une route vide dans le composant lazy loader et que on charge déja le composant au niveau du routing du parent, on aura un double affichage.
   // Dans notre cas on a préféré charger le composant au niveau du parent (ici, component : MemberAreaComponent) donc on ne va pas mettre de chemin vide
   // "path : "", component : ... dans notre enfant (member-area-routing)
-  { path : "public-area", component : PublicAreaComponent , loadChildren : () => import("./features/public-area/public-area.module").then(m => m.PublicAreaModule) },
   { path : "admin-area", component : AdminAreaComponent , loadChildren : () => import("./features/admin-area/admin-area.module").then(m => m.AdminAreaModule) }
 ];
 
--- src/app/features/public-area/public-area-routing.module.ts
+++ src/app/features/public-area/public-area-routing.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
-/* Création du routing-module
- * Ce `routing module` a été généré automatiquement par la commande :
- * 
- *    ng g m [nom-du-sous-dossier] --module=[nom-du-module-parent] --routing
- * 
- * Pour des instructions détaillées sur la création de ce module et la commande utilisée,
- * veuillez vous référer au commentaire situé dans le fichier du module associé.
- */
-
+import { LoginComponent } from './login/login.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  { path : "home", component : HomeComponent}
+  { path: '', component: HomeComponent }, 
+  { path: 'login', component: LoginComponent },
+  { path: 'pricing', component: PricingComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
