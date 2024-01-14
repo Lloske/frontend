cf415901da6fc79051b88addb3433dd557dab5c9
AntoTilm <antoine.tilman@gmail.com>
2024-01-14 18:11:43 +0100

    Formulaire hmtl contractual data ok

--- src/app/features/member-area/employees/shared-employees/models/contractual-data.ts
+++ src/app/features/member-area/employees/shared-employees/models/contractual-data.ts
@@ -1,14 +1,14 @@
 export interface ContractualData {
     Id : number;
-    Contract_type : number;
-    Employment_type : number;
+    Contract_type : string;
+    Employment_type : string;
     Job_title : string;
     Organization_entry_date : Date;
     Contract_start : Date;
     Probation_end_date : Date;
     Contract_end : Date;
-    Status : number;
-    Professional_category : number;
+    Status : string;
+    Professional_category : string;
     Last_medical_checkup_date : string;
-    FK_id_user_personnal_information : string;
+    FK_id_user_personnal_information : number;
 }
