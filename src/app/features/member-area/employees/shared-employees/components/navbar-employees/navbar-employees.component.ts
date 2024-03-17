74a82b8467f20a2b249dfd3ed79ab91be79eef3a
AntoTilm <antoine.tilman@gmail.com>
2024-03-17 16:57:45 +0100

    modale addEmloyees fonctionnelle

--- src/app/features/member-area/employees/shared-employees/components/navbar-employees/navbar-employees.component.ts
+++ src/app/features/member-area/employees/shared-employees/components/navbar-employees/navbar-employees.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-employees',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar-employees.component.scss']
 })
 export class NavbarEmployeesComponent {
+  
   active = 1;
+  
+  displayStatus = 'none';
+
+  addEmployeeModal() {  
+    this.displayStatus = 'block';
+  }
+
+  updateEmployee() {
+    console.log('A CODER');
+  }
+
+  closeModal() {
+    this.displayStatus = 'none';
+  }
+
+  // This function will be called whenever a click event is fired in the document
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const modal = document.getElementById('myModal');
+    if (event.target === modal) {
+      this.closeModal();
+    }
+  }
 }
